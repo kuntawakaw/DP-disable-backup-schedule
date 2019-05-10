fbf5aab9cc9e32b37e9fc046360ae8cc5569da95
kuntawakaw <kuntawakaw@gmail.com>
2019-05-11 02:47:30 +0800

    Update disable_backup.ps1

--- disable_backup.ps1
+++ disable_backup.ps1
@@ -96,7 +96,7 @@ function Sched-disable-info
 #write-host "You Selected $script:cellname"
 write-host " "
 $script:specname = Read-host -Prompt 'Enter spec name. eg : gvu0081_TIAP'
-$speclist = (.\plink.exe -ssh -l aqmalscr -i .\aqmalbkp.ppk $script:cellname "ls -d /etc/opt/omni/server/schedules/* |grep -i $script:specname ; ls -d /etc/opt/omni/server/barschedules/oracle8/* |grep -i $script:specname ;ls -d /etc/opt/omni/server/barschedules/mssql/* |grep -i $script:specname") 
+$speclist = (.\plink.exe -ssh -l $UNIXUSER -i .\$PUBKEY $script:cellname "ls -d /etc/opt/omni/server/schedules/* |grep -i $script:specname ; ls -d /etc/opt/omni/server/barschedules/oracle8/* |grep -i $script:specname ;ls -d /etc/opt/omni/server/barschedules/mssql/* |grep -i $script:specname") 
 if (!$speclist) {Write-Host "Spec not found... Exiting" ; Start-Sleep -s 10 ; exit }
 Write-output $speclist |out-file specfile.txt
 Import-Csv .\specfile.txt -header c1,c2,c3,c4,c5,c6,c7,c8 -Delimiter "/"  |Select-Object c7,c8 |Format-Table -HideTableHeaders
@@ -126,19 +126,19 @@ else  {write-host "Unrecognized. exiting"}
 function disable-backup-permanent
 {
 $date_now=(Get-Date -UFormat "%m%d%Y_%H%M%p")
-gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cp' + " " + $_ + " " + '/home/aqmalscr/sched/' +($_.Split("/")[6,7] -join "_") + "_" + $date_now + ' ; else echo " No Read Permission ' + ($_.Split("/")[6,7] -join " ")+ '" ; fi' } |Out-File .\cmd.txt                                                                   #copy schedule to homedir
-#gc .\specfile.txt |ForEach-Object{ (gc .\perl.txt)[0] + "/home/aqmalscr/sched/" + ($_.Split("/")[4,5] -join "_") + "_" +$script:date_now + " | tee " + $_ } |Out-File -Append .\cmd.txt                                          #remove existing disable
-#gc .\specfile.txt |ForEach-Object{ (gc .\perl.txt)[1] + "/home/aqmalscr/sched/" + ($_.Split("/")[4,5] -join "_") + "_" +$script:date_now + " | tee " + $_ } |Out-File -Append .\cmd.txt                                          #remove existing starting
-gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cat ' + $_ + (gc .\perl.txt)[7] + " | tee /home/aqmalscr/schedtmp/" + ($_.Split("/")[6,7] -join "_") + "_" +$date_now + ' > /dev/null' + ' ; else echo "Schedule not modified" ; fi ' } | Out-File -Append .\cmd.txt                  #grep -v disable and starting and output to temp file
-gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + (gc .\perl.txt)[2] + "/home/aqmalscr/schedtmp/" + ($_.Split("/")[6,7] -join "_") + "_" +$date_now + " | /opt/pb/bin/pbrun tee " + $_ + ' > /dev/null 2>&1 ; echo "Disable Successful" '  + ' ; else echo "Please check file permission" ; fi ' } |Out-File -Append .\cmd.txt                                          #add disable to first line
-#gc .\specfile.txt |ForEach-Object{ 'if cmp -s ' + $_ + '/home/aqmalscr/sched/' +($_.Split("/")[6,7] -join "_") + "_" + $date_now + ' ; then echo "Modify Success" ; else echo "Modify Failed" ; fi' } | Out-File -Append .\cmd.txt
+gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cp' + " " + $_ + " " + '/home/$UNIXUSER/sched/' +($_.Split("/")[6,7] -join "_") + "_" + $date_now + ' ; else echo " No Read Permission ' + ($_.Split("/")[6,7] -join " ")+ '" ; fi' } |Out-File .\cmd.txt                                                                   #copy schedule to homedir
+#gc .\specfile.txt |ForEach-Object{ (gc .\perl.txt)[0] + "/home/$UNIXUSER/sched/" + ($_.Split("/")[4,5] -join "_") + "_" +$script:date_now + " | tee " + $_ } |Out-File -Append .\cmd.txt                                          #remove existing disable
+#gc .\specfile.txt |ForEach-Object{ (gc .\perl.txt)[1] + "/home/$UNIXUSER/sched/" + ($_.Split("/")[4,5] -join "_") + "_" +$script:date_now + " | tee " + $_ } |Out-File -Append .\cmd.txt                                          #remove existing starting
+gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cat ' + $_ + (gc .\perl.txt)[7] + " | tee /home/$UNIXUSER/schedtmp/" + ($_.Split("/")[6,7] -join "_") + "_" +$date_now + ' > /dev/null' + ' ; else echo "Schedule not modified" ; fi ' } | Out-File -Append .\cmd.txt                  #grep -v disable and starting and output to temp file
+gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + (gc .\perl.txt)[2] + "/home/$UNIXUSER/schedtmp/" + ($_.Split("/")[6,7] -join "_") + "_" +$date_now + " | /opt/pb/bin/pbrun tee " + $_ + ' > /dev/null 2>&1 ; echo "Disable Successful" '  + ' ; else echo "Please check file permission" ; fi ' } |Out-File -Append .\cmd.txt                                          #add disable to first line
+#gc .\specfile.txt |ForEach-Object{ 'if cmp -s ' + $_ + '/home/$UNIXUSER/sched/' +($_.Split("/")[6,7] -join "_") + "_" + $date_now + ' ; then echo "Modify Success" ; else echo "Modify Failed" ; fi' } | Out-File -Append .\cmd.txt
 Add-Content -value "/opt/omni/bin/omnirpt -rep dl_sched -tab |grep $script:specname" -path .\cmd.txt
 [System.Environment]::CurrentDirectory = (Get-Location).Path
 $MyPath = ".\cmd.txt"
 $MyFile = Get-Content $MyPath
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
 [System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
-(.\plink.exe -ssh -l aqmalscr -i .\aqmalbkp.ppk $script:cellname -m .\cmd.txt)
+(.\plink.exe -ssh -l $UNIXUSER -i .\$PUBKEY $script:cellname -m .\cmd.txt)
 }
 
 
@@ -147,18 +147,18 @@ function disable-backup-day
 $script:newdate = get-date((get-date).addDays($daysdisable)) -uformat "%d %m %Y"
 $date_now=(Get-Date -UFormat "%m%d%Y_%H%M%p")
 Write-Output $script:newdate | Out-File .\newdate.txt
-gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cp' + " " + $_ + " " + '/home/aqmalscr/sched/' +($_.Split("/")[6,7] -join "_") + "_" + $date_now + ' ; else echo " No Read Permission ' + ($_.Split("/")[6,7] -join " ")+ '" ; fi' } |Out-File .\cmd.txt                                                                   #copy schedule to homedir
+gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cp' + " " + $_ + " " + '/home/$UNIXUSER/sched/' +($_.Split("/")[6,7] -join "_") + "_" + $date_now + ' ; else echo " No Read Permission ' + ($_.Split("/")[6,7] -join " ")+ '" ; fi' } |Out-File .\cmd.txt                                                                   #copy schedule to homedir
 #gc .\specfile.txt |ForEach-Object{ (gc .\perl.txt)[0] + "/home/aqmalbkp/sched/" + ($_.Split("/")[4,5] -join "_") + "_" +$script:date_now + " | tee " + $_ } |Out-File -Append .\cmd.txt                                          #remove existing disable
 #gc .\specfile.txt |ForEach-Object{ (gc .\perl.txt)[1] + "/home/aqmalbkp/sched/" + ($_.Split("/")[4,5] -join "_") + "_" +$script:date_now + " | tee " + $_ } |Out-File -Append .\cmd.txt                                          #remove existing starting
-gc .\specfile.txt |ForEach-Object{'if [ -r ' + $_ + ' ] ; then ' + 'cat ' + $_ + (gc .\perl.txt)[7] + " | tee /home/aqmalscr/schedtmp/" + ($_.Split("/")[6,7] -join "_") + "_" +$date_now + ' > /dev/null'  + ' ; else echo "Schedule not modified" ; fi ' } | Out-File -Append .\cmd.txt                  #grep -v disable and starting and output to temp file
-gc .\specfile.txt |ForEach-Object{'if [ -r ' + $_ + ' ] ; then ' + (gc .\perl.txt)[5] + $script:newdate + (gc .\perl.txt)[6] + "/home/aqmalscr/schedtmp/" + ($_.Split("/")[6,7] -join "_") + "_" + $date_now + " | /opt/pb/bin/pbrun tee " + $_  + ' > /dev/null 2>&1 ; echo "Disable Successful" '  + ' ; else echo "Please check file permission" ; fi '} |Out-File -Append .\cmd.txt    #adding starting from temp file to original file
+gc .\specfile.txt |ForEach-Object{'if [ -r ' + $_ + ' ] ; then ' + 'cat ' + $_ + (gc .\perl.txt)[7] + " | tee /home/$UNIXUSER/schedtmp/" + ($_.Split("/")[6,7] -join "_") + "_" +$date_now + ' > /dev/null'  + ' ; else echo "Schedule not modified" ; fi ' } | Out-File -Append .\cmd.txt                  #grep -v disable and starting and output to temp file
+gc .\specfile.txt |ForEach-Object{'if [ -r ' + $_ + ' ] ; then ' + (gc .\perl.txt)[5] + $script:newdate + (gc .\perl.txt)[6] + "/home/$UNIXUSER/schedtmp/" + ($_.Split("/")[6,7] -join "_") + "_" + $date_now + " | /opt/pb/bin/pbrun tee " + $_  + ' > /dev/null 2>&1 ; echo "Disable Successful" '  + ' ; else echo "Please check file permission" ; fi '} |Out-File -Append .\cmd.txt    #adding starting from temp file to original file
 Add-Content -value "/opt/omni/bin/omnirpt -rep dl_sched -tab |grep $script:specname" -path .\cmd.txt
 [System.Environment]::CurrentDirectory = (Get-Location).Path
 $MyPath = ".\cmd.txt"
 $MyFile = Get-Content $MyPath
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
 [System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
-(.\plink.exe -ssh -l aqmalscr -i .\aqmalbkp.ppk $script:cellname -m .\cmd.txt)
+(.\plink.exe -ssh -l $UNIXUSER -i .\$PUBKEY $script:cellname -m .\cmd.txt)
 }
 
 function enable-backup
@@ -166,7 +166,7 @@ function enable-backup
 #write-host "You Selected $script:cellname"
 write-host " "
 $script:specname = Read-host -Prompt 'Spec name'
-$speclist = (.\plink.exe -ssh -l aqmalscr -i .\aqmalbkp.ppk $script:cellname "ls -d /etc/opt/omni/server/schedules/* |grep -i $script:specname ; ls -d /etc/opt/omni/server/barschedules/oracle8/* |grep -i $script:specname ;ls -d /etc/opt/omni/server/barschedules/mssql/* |grep -i $script:specname ") 
+$speclist = (.\plink.exe -ssh -l $UNIXUSER -i .\$PUBKEY $script:cellname "ls -d /etc/opt/omni/server/schedules/* |grep -i $script:specname ; ls -d /etc/opt/omni/server/barschedules/oracle8/* |grep -i $script:specname ;ls -d /etc/opt/omni/server/barschedules/mssql/* |grep -i $script:specname ") 
 if (!$speclist) {Write-Host "Spec not found. Exiting "; Start-Sleep -s 5 ; exit }
 Write-output $speclist |out-file specfile.txt
 Import-Csv .\specfile.txt -header c1,c2,c3,c4,c5,c6,c7,c8 -Delimiter "/"  |Select-Object c7,c8 |Format-Table -HideTableHeaders
@@ -178,17 +178,17 @@ if ( $response -ne "Y" ) { exit }
 write-host "==================================="
 
 $date_now=(Get-Date -UFormat "%m%d%Y_%H%M%p")
-gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cp' + " " + $_ + " " + '/home/aqmalscr/sched/' +($_.Split("/")[6,7] -join "_") + "_" + $date_now + ' ; else echo " No Read Permission ' + ($_.Split("/")[6,7] -join " ")+ '" ; fi' } |Out-File .\cmd.txt                                                                   #copy schedule to homedir
+gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cp' + " " + $_ + " " + '/home/$UNIXUSER/sched/' +($_.Split("/")[6,7] -join "_") + "_" + $date_now + ' ; else echo " No Read Permission ' + ($_.Split("/")[6,7] -join " ")+ '" ; fi' } |Out-File .\cmd.txt                                                                   #copy schedule to homedir
 #gc .\specfile.txt |ForEach-Object{ (gc .\perl.txt)[0] + "/home/aqmalbkp/sched/" + ($_.Split("/")[4,5] -join "_") + "_" + $script:date_now + " | tee " + $_  } |Out-File -Append .\cmd.txt                                          #remove existing disable
 #gc .\specfile.txt |ForEach-Object{ (gc .\perl.txt)[1] + "/home/aqmalbkp/sched/" + ($_.Split("/")[4,5] -join "_") + "_" + $script:date_now + " | tee " + $_  } |Out-File -Append .\cmd.txt                                          #remove existing starting
-gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cat ' + '/home/aqmalscr/sched/' + ($_.Split("/")[6,7] -join "_") + "_" + $:date_now + (gc .\perl.txt)[7] + " | /opt/pb/bin/pbrun tee " + $_ + ' > /dev/null 2>&1 ; echo "Enable Successful" '  + ' ; else echo "Schedule not modified" ; fi ' } | Out-File -Append .\cmd.txt
+gc .\specfile.txt |ForEach-Object{ 'if [ -r ' + $_ + ' ] ; then ' + 'cat ' + '/home/$UNIXUSER/sched/' + ($_.Split("/")[6,7] -join "_") + "_" + $:date_now + (gc .\perl.txt)[7] + " | /opt/pb/bin/pbrun tee " + $_ + ' > /dev/null 2>&1 ; echo "Enable Successful" '  + ' ; else echo "Schedule not modified" ; fi ' } | Out-File -Append .\cmd.txt
 Add-Content -value "/opt/omni/bin/omnirpt -rep dl_sched -tab |grep $script:specname" -path .\cmd.txt
 [System.Environment]::CurrentDirectory = (Get-Location).Path
 $MyPath = ".\cmd.txt"
 $MyFile = Get-Content $MyPath
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
 [System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
-(.\plink.exe -ssh -l aqmalscr -i .\aqmalbkp.ppk $script:cellname -m .\cmd.txt)
+(.\plink.exe -ssh -l $UNIXUSER -i .\$PUBKEY $script:cellname -m .\cmd.txt)
 }
 
 
